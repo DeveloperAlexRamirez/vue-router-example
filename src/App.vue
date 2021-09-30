3256bc19f54a4da960cc1f6ab2a8921b98b8661a
DeveloperAlexRamirez <alexkey0516@gmail.com>
2021-09-29 19:14:39 -0500

    Life cycles hooks

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <div class="app__contenedorLogo">
       <img src="./assets/logo.png" />
     </div>
+  </div>
+
+  <div class="router__contenedor">
     <router-view />
   </div>
 </template>
@@ -33,5 +36,12 @@ export default {
 
 .app__contenedorLogo {
   max-width: 120rem;
+}
+
+.router__contenedor {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,74 @@
 <template>
   <h1>About page</h1>
+  <h3>{{ name }}</h3>
+  <button @click="onChangeName">Change name</button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Alex',
+    };
+  },
+
+  methods: {
+    onChangeName() {
+      this.name = 'Ramírez';
+    },
+  },
+
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+
+  created() {
+    console.log('created');
+    // Es buen lugar para Peticiones HTTP
+  },
+
+  beforeMount() {
+    console.log('beforeMount');
+  },
+
+  mounted() {
+    console.log('mounted');
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+
+  updated() {
+    console.log('updated');
+  },
+
+  activated() {
+    console.log('activated');
+  },
+
+  deactivated() {
+    console.log('deactivated');
+  },
+
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+
+  unmounted() {
+    console.log('unmounted');
+  },
+
+  errorCaptured() {
+    console.log('errorCaptured');
+  },
+
+  renderTracked() {
+    console.log('renderTracked');
+  },
+
+  renderTriggered() {
+    console.log('renderTriggered');
+  },
+};
+</script>
