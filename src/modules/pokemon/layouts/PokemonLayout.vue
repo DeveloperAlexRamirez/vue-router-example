00411643fdbd499e07502f81abcfdaa75f941f06
DeveloperAlexRamirez <alexkey0516@gmail.com>
2021-10-21 09:10:40 -0500

    Children Routes

---
+++ src/modules/pokemon/layouts/PokemonLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pokemon-layout">
+    <h1>Pokemon Layout</h1>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.pokemon-layout {
+  background-color: deeppink;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
